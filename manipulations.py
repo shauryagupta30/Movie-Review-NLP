b1cc626fbde4db488f7a325f90d50b5afe6c8758
shauryagupta30 <41203937+shauryagupta30@users.noreply.github.com>
2020-07-29 03:08:19 +0530

    Create manipulations.py

---
+++ manipulations.py
@@ -0,0 +1,7 @@
+stop_words = list(set(stopwords.words('english')))
+print(stop_words)
+#j is adjective, r is adverb, and v is verb
+#allowed_word_types = ["J","R","V"]
+allowed_word_types = ["J"]
+all_words = []
+documents = []
