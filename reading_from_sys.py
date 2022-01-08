bba070a4ba495ea0f3fcd5b4ecac1232239e9dd5
Shaurya Gupta <41203937+shauryagupta30@users.noreply.github.com>
2022-01-08 20:42:19 +0530

    Delete reading_from_sys.py

--- reading_from_sys.py
+++
@@ -1,6 +0,0 @@
-import os
-files_pos = os.listdir(r'C:/Users/dell/Desktop/train_pos')
-files_neg = os.listdir(r'C:/Users/dell/Desktop/train_neg')
-files_pos = [open(r'C:/Users/dell/Desktop/train_pos/'+f, 'r',encoding="utf8").read() for f in files_pos]
-
-files_neg = [open(r'C:/Users/dell/Desktop/train_neg/'+f, 'r',encoding="utf8").read() for f in files_neg]
