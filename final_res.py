2d380da5b254b623edbe4f2402e81cad3e3591c9
Shaurya Gupta <41203937+shauryagupta30@users.noreply.github.com>
2022-01-08 20:41:29 +0530

    Delete final_res.py

--- final_res.py
+++
@@ -1,5 +0,0 @@
-classifier = nltk.NaiveBayesClassifier.train(training_set)
-t=(nltk.classify.accuracy(classifier, testing_set))*100
-y.append(t)
-print("Classifier accuracy percent:",t)
-classifier.show_most_informative_features(15)
